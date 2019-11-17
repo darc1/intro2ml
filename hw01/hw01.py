e2cb65649f8839eea420c2a42cb6b897bab93ba2
Chen Dar <cdoar@vmware.com>
2019-11-17 00:56:03 -0800

    new behaviur to parallel return

---
+++ hw01/hw01.py
@@ -0,0 +1,86 @@
+import json
+
+import numpy
+from joblib import Parallel, delayed
+from sklearn.datasets import fetch_openml
+import matplotlib.pyplot as plt
+
+from nn import knn
+
+mnist = fetch_openml('mnist_784')
+data = mnist['data']
+labels = mnist['target']
+
+idx = numpy.random.RandomState(0).choice(70000, 11000)
+train = data[idx[:10000], :].astype(int)
+train_labels = labels[idx[:10000]]
+test = data[idx[10000:], :].astype(int)
+test_labels = labels[idx[10000:]]
+
+
+def run_for_k(k, n):
+    print(f"running kNN for {n} images with k={k}")
+    correct_predictions = 0
+    for i in range(n):
+        prediction = knn(train, train_labels, test[i], k=k)
+        if prediction == test_labels[i]:
+            correct_predictions += 1
+
+    print(f"number of correct predictions: {correct_predictions}")
+    correct_predictions_rate = float(correct_predictions) / float(n)
+    print(f"correct predication percentage for k:{k} is: {correct_predictions_rate}")
+    return correct_predictions_rate
+
+
+def run_job(key, k, n):
+    return {key: run_for_k(k=k, n=n)}
+
+
+def return_results(results, file_name=None):
+    if file_name is None:
+        return results
+
+    with open(file_name, "w+") as w:
+        w.write(json.dumps(results))
+
+
+def create_graph(results, x_label, y_label, file_name):
+    x_values = list(results.keys())
+    x_values.sort()
+    y_values = [results[x] for x in x_values]
+    plt.plot(x_values, y_values)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.savefig(file_name)
+
+
+def section_b(file_name=None):
+    print("Running section (b)")
+    result = {"prediction_rate": run_for_k(10, 1000)}
+    return return_results(results=result, file_name=file_name)
+
+
+def section_c(file_name=None):
+    print("Running section (c)")
+
+    results = Parallel(n_jobs=16)(delayed(run_job)(k, k, 1000) for k in range(1, 101))
+    print(results)
+
+    create_graph(results, "n", "error rate", "section-c.png")
+
+    return return_results(results=results, file_name=file_name)
+
+
+def section_d(file_name=None):
+    print("Running section (d)")
+
+    results = Parallel(n_jobs=16)(delayed(run_job)(n, 1, n) for n in range(100, 5100, 100))
+    print(results)
+    create_graph(results, "k", "error rate", "section-d.png")
+
+    return return_results(results=results, file_name=file_name)
+
+
+# section_b(file_name="sectionB.json")
+section_c(file_name="sectionC.json")
+section_d(file_name="sectionD.json")
--- hw01/nn.py
+++ hw01/nn.py
@@ -1,88 +1,19 @@
-from sklearn.datasets import fetch_openml
-import numpy.random
 import numpy
-import json
-from joblib import Parallel, delayed
-
-mnist = fetch_openml('mnist_784')
-data = mnist['data']
-labels = mnist['target']
-
-idx = numpy.random.RandomState(0).choice(70000, 11000)
-train = data[idx[:10000], :].astype(int)
-train_labels = labels[idx[:10000]]
-test = data[idx[10000:], :].astype(int)
-test_labels = labels[idx[10000:]]
 
 
-def knn(images, image_labels, query_image, k):
-    distances = numpy.zeros(len(images))
+def knn(train_images, labels, testimage, k):
+    distances = numpy.zeros(len(train_images))
 
-    for i in range(len(images)):
+    for i in range(len(train_images)):
         # calculate Euclidean distance
-        dist = numpy.linalg.norm(images[i] - query_image)
+        dist = numpy.linalg.norm(train_images[i] - testimage)
         distances[i] = dist
 
     # find k nearest distances index labels
     k_nearest_idx = numpy.argpartition(distances, k)
-    k_nearest_labels = image_labels[k_nearest_idx[:k]]
+    k_nearest_labels = labels[k_nearest_idx[:k]]
 
     # count labels occurrences from k nearest and return label with max
     (values, counts) = numpy.unique(k_nearest_labels, return_counts=True)
     nearest_label_idx = numpy.argmax(counts)
     return values[nearest_label_idx]
-
-
-def run_for_k(k, n):
-    print(f"running kNN for {n} images with k={k}")
-    correct_predictions = 0
-    for i in range(n):
-        prediction = knn(train, train_labels, test[i], k=k)
-        if prediction == test_labels[i]:
-            correct_predictions += 1
-
-    print(f"number of correct predictions: {correct_predictions}")
-    correct_predictions_rate = float(correct_predictions) / float(n)
-    print(f"correct predication percentage for k:{k} is: {correct_predictions_rate}")
-    return correct_predictions_rate
-
-def run_in_parallel(results, key, k, n):
-    results[key] = run_for_k(k=k, n=n)
-
-def return_results(results, file_name=None):
-    if file_name is None:
-        return results
-
-    with open(file_name, "w+") as w:
-        w.write(json.dumps(results))
-
-
-def section_b(file_name=None):
-    print("Running section (b)")
-    result = {"prediction_rate": run_for_k(10, 1000)}
-    return return_results(results=result, file_name=file_name)
-
-
-def section_c(file_name=None):
-    print("Running section (c)")
-
-    results = {}
-    Parallel(n_jobs=16)(delayed(run_in_parallel)(results, k, k, 1000) for k in range(1, 101))
-
-    print(f"{results}")
-    return return_results(results=results, file_name=file_name)
-
-
-def section_d(file_name=None):
-    print("Running section (d)")
-
-    results = {}
-    Parallel(n_jobs=16)(delayed(run_in_parallel)(results, n, 1, n) for n in range(100, 5100, step=100))
-
-    print(f"{results}")
-    return return_results(results=results, file_name=file_name)
-
-
-section_b(file_name="sectionB.json")
-section_c(file_name="sectionC.json")
-section_d(file_name="sectionD.json")
